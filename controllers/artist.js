2aa9e56b2fb2b5104ef0c9cbc3a4bd9e2d86eabd
Mario <mariogv87@gmail.com>
2017-11-26 21:49:13 +0100

    Cargar y leer imágenes de artista

--- controllers/artist.js
+++ controllers/artist.js
@@ -6,6 +6,7 @@ var mongoosePaginate = require('mongoose-pagination');
 var Artist = require('../models/artist');
 var Album = require('../models/album');
 var Song = require('../models/song');
+var file = require('../services/file');
 
 function getArtist(req, res){
 	var artistId = req.params.id;
@@ -124,10 +125,60 @@ function deleteArtist(req, res){
 	});
 }
 
+function uploadImage(req, res){
+	var artistId = req.params.id;
+	var file_name = 'No subido ...';
+
+	if (req.files) {
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('\/');
+		var file_name = file_split[2];
+		
+		var ex_split = file_path.split('\.');
+		var file_ex = ex_split[1];
+
+		if(file_ex == 'png' || file_ex == 'jpg' || file_ex == 'gif'){
+			Artist.findByIdAndUpdate(artistId,{image: file_name},(err, artistUpdated) =>{
+				if (err) {
+					console.log(err);
+					res.status(500).send({messaje: 'Error al actualizar la imagen'});
+				}else{
+					if (!artistUpdated) {
+						res.status(404).send({messaje: 'No se ha podido actualizar la imagen'});
+					}else{
+						res.status(200).send({
+							artist: artistUpdated
+						});
+					}
+				}
+			});
+		}else{
+			file.deleteFile(file_path);
+			res.status(200).send({messaje: 'Extensión del archivo no válido'});
+		}
+	}else{
+		res.status(200).send({messaje: 'No ha subido ninguna imagen'});
+	}
+}
+
+function getImageFile(req, res) {
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/artists/' + imageFile;
+	fs.exists(path_file, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(404).send({messaje: 'No existe la imagen...'});			
+		}
+	});
+}
+
 module.exports = {
 	getArtist,
 	saveArtist,
 	getArtists,
 	updateArtist,
-	deleteArtist
+	deleteArtist,
+	uploadImage,
+	getImageFile
 };
