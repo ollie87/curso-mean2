684abdedcd8438e5a7629fae2f9d4b867c084569
Mario <mariogv87@gmail.com>
2017-11-27 18:21:37 +0100

    Album Controller
    Metodos y rutas para obtener un album por id y guardar un album

---
+++ controllers/album.js
@@ -0,0 +1,54 @@
+'use strict'
+
+var fs = require('fs');
+var path = require('path');
+var mongoosePaginate = require('mongoose-pagination');
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
+var file = require('../services/file');
+
+function getAlbum(req, res){
+	var albumId = req.params.id;
+
+	Album.findById(albumId, (err,album) =>{
+		if(err){
+			res.status(500).send({message: 'Error en la petición getAlbum'});
+		}else{
+			if (!album) {
+				res.status(404).send({message: 'El album no existe'});	
+			}else{
+				res.status(200).send({album: album});	
+			}
+		}
+	});
+}
+
+function saveAlbum(req, res){
+	var album = new Album();
+
+	var params = req.body;
+
+	album.title = params.title;
+	album.description = params.description;
+	album.year = params.year;
+	album.image = 'null';
+	album.artist = params.artist;
+
+	album.save((err, albumtStored) => {
+		if(err){
+			res.status(500).send({message: 'Error al guardar el album'});
+		}else{
+			if (!albumtStored) {
+				res.status(404).send({message: 'El album no ha sido guardado'});	
+			}else{
+				res.status(200).send({album: albumtStored});	
+			}
+		}
+	})
+}
+
+module.exports = {
+	getAlbum,
+	saveAlbum
+}
