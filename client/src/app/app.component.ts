3f586a0c1db284956b73b98bcfdd911eb5ac203b
Mario <mariogv87@gmail.com>
2017-12-09 17:25:15 +0100

    Commit inicial Angular
    Creación de proyecto Angular con sus dependencias

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+//ng serve --host 10.100.1.10 Para arrancar la aplicación
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MUSIFY';
+}
