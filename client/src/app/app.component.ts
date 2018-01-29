83965d0959ee02ccb01fe41ad7d6a50ffcdbff3b
Mario <mariogv87@gmail.com>
2018-01-29 19:16:39 +0100

    Cambios en app.component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 import { User } from './models/user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   public title = 'MUSIFY';
   public user: User;
-	public identity;
-	public token;
+  public identity;
+  public token;
 
-	constructor(){
-		this.user = new User('','','','','','ROLE_USER','');
-	}
+  constructor(){
+  	this.user = new User('','','','','','ROLE_USER','');
+  }
 
-	public onSubmit(){
-		console.log(this.user);
-	}
-
-	
+  public onSubmit(){
+  	console.log(this.user);
+  }
 
 }
