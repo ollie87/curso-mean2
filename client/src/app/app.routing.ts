04feab79c57a508c5f70a9c03ec75b0d1c6464f0
Mario <mariogv87@gmail.com>
2018-02-12 18:54:46 +0100

    Marcar menú artista y mis datos cuando está activo.

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -2,11 +2,14 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserEditComponent} from './components/user-edit.component';
+import { ArtistListComponent} from './components/artist-list.component';
 
 const appRoutes: Routes = [
-	{path: '', component: UserEditComponent},
+	{path: '', redirectTo: '/artists/1', pathMatch: 'full'},
+	//{path: '', component: ArtistListComponent},
 	{path: 'mis-datos', component: UserEditComponent},
-	{path: '**', component: UserEditComponent}
+	{path: 'artists/:page', component: ArtistListComponent},
+	{path: '**', redirectTo: '/artists/1'}
 ];
 
 export const appRoutingProviders: any[] = [];
