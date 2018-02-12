f881ed51e478f0b5fecebffadee485a3df61bb9a
Mario <mariogv87@gmail.com>
2018-02-12 18:52:53 +0100

    Creamos la base para los artistas

---
+++ client/src/app/components/artist-list.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../services/user.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {Artist} from '../models/artist';
+import {GLOBAL} from '../services/global';
+
+@Component({
+	selector: 'artist-list',
+	templateUrl: '../views/artist-list.html',
+	providers: [UserService]
+})
+
+export class ArtistListComponent implements OnInit {
+	public titulo: string;
+	public artist: Artist[];
+	public identity;
+	public token;
+	public url: string;
+
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private _userService: UserService
+	) {
+		this.titulo = 'Artistas';
+		this.identity = this._userService.getIdentity();
+		this.token = this._userService.getToken();
+		this.url = GLOBAL.url;
+	}
+
+	ngOnInit(){
+		console.log('artist-list.component.ts cargado');
+	}
+}
