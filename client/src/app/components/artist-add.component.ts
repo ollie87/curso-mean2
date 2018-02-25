471a8981900c73c3da6dd1fb1b850b9951c6ec80
Mario <mariogv87@gmail.com>
2018-02-25 12:49:07 +0100

    Componente artist-add
    Se añade a mayores la vista para el listado de artistas

---
+++ client/src/app/components/artist-add.component.ts
@@ -0,0 +1,59 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../services/user.service';
+import {ArtistService} from '../services/artist.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {Artist} from '../models/artist';
+import {GLOBAL} from '../services/global';
+
+@Component({
+	selector: 'artist-add',
+	templateUrl: '../views/artist-add.html',
+	providers: [UserService,ArtistService]
+})
+
+export class ArtistAddComponent implements OnInit {
+	public titulo: string;
+	public artist: Artist;
+	public identity;
+	public token;
+	public url: string;
+	public alertMessage;
+
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private _userService: UserService,
+		private _artistService: ArtistService
+	) {
+		this.titulo = 'Crear nuevo artista';
+		this.identity = this._userService.getIdentity();
+		this.token = this._userService.getToken();
+		this.url = GLOBAL.url;
+		this.artist = new Artist('','','');
+	}
+
+	ngOnInit(){
+		console.log('artist-add.component.ts cargado');
+	}
+	onSubmit(){
+		console.log(this.artist);
+		this._artistService.addArtist(this.token,this.artist).subscribe(
+			response => {
+				if (!response.artist) {
+					this.alertMessage = 'Error en el servidor';
+				}else{
+					this.alertMessage = 'El artista se ha creado correctamente';
+					this.artist = response.artist;
+					//this._router.navigate(['/editar-artista'], response.artist._id);
+				}
+			},
+			error =>{
+	  			if (error != null) {
+	  				var body = JSON.parse(error._body);
+	  				this.alertMessage = body.messaje;
+	  				console.log(error);
+	  			}
+	  		}
+		)
+	}
+}
