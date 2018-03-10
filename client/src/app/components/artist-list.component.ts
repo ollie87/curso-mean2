06f88c85264f5911b9d0e93444b2c6977ef69bb9
Mario <mariogv87@gmail.com>
2018-03-10 11:51:26 +0100

    Maquedato Listado Artistas

--- client/src/app/components/artist-list.component.ts
+++ client/src/app/components/artist-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../services/user.service';
+import {ArtistService} from '../services/artist.service';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {Artist} from '../models/artist';
 import {GLOBAL} from '../services/global';
@@ -7,28 +8,70 @@ import {GLOBAL} from '../services/global';
 @Component({
 	selector: 'artist-list',
 	templateUrl: '../views/artist-list.html',
-	providers: [UserService]
+	providers: [UserService, ArtistService]
 })
 
 export class ArtistListComponent implements OnInit {
 	public titulo: string;
-	public artist: Artist[];
+	public artists: Artist[];
 	public identity;
 	public token;
 	public url: string;
+	public next_page;
+	public prev_page;
+	public alertMessage: string;
+	public classAlertMessage: string;
 
 	constructor(
 		private _route: ActivatedRoute,
 		private _router: Router,
-		private _userService: UserService
+		private _userService: UserService,
+		private _artistService: ArtistService
+
 	) {
 		this.titulo = 'Artistas';
 		this.identity = this._userService.getIdentity();
 		this.token = this._userService.getToken();
 		this.url = GLOBAL.url;
+		this.next_page = 1;
+		this.prev_page = 1;
 	}
 
 	ngOnInit(){
 		console.log('artist-list.component.ts cargado');
+		this.getArtists();
+	}
+
+	getArtists(){
+		this._route.params.forEach((params: Params) => {
+			let page = +params['page'];
+			if (!page) {
+				page == 1;
+			}else{
+				this.next_page = page +1;
+				this.prev_page = page -1;				
+				if (this.prev_page == 0) {
+					this.prev_page = 1;
+				}
+			}
+
+			this._artistService.getArtists(this.token,page).subscribe(
+				response =>{
+					if (!response.artists) {
+						this._router.navigate(['/']);
+					}else{
+						this.artists = response.artists;
+					}
+				},
+				error =>{
+		  			if (error != null) {
+		  				var body = JSON.parse(error._body);
+		  				this.alertMessage = body.messaje;
+		  				this.classAlertMessage = 'alert alert-danger';
+		  				console.log(error);
+		  			}
+	  			}
+			)
+		});
 	}
 }
