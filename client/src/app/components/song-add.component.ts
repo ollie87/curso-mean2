07f0a9de63dc416685a5a7405234ecbdc1667f47
Mario <mariogv87@gmail.com>
2018-05-03 18:48:41 +0200

    Ruta editar_tema

--- client/src/app/components/song-add.component.ts
+++ client/src/app/components/song-add.component.ts
@@ -51,7 +51,7 @@ export class SongAddComponent implements OnInit {
 					this.alertMessage = 'La canción se ha creado correctamente';
 					this.song = response.song;
 					this.classAlertMessaje = 'alert alert-info';
-					//this._router.navigate(['/editar_album/' + response.song._id]);
+					this._router.navigate(['/editar_tema/' + response.song._id]);
 				}
 			},
 			error =>{
