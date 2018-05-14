eb6bef2ce477d93c0d2e5d4827c88025884be145
Mario <mariogv87@gmail.com>
2018-05-14 12:21:24 +0200

    Player Component && view changes

---
+++ client/src/app/components/player.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {Song} from '../models/song';
+import {GLOBAL} from '../services/global';
+
+
+@Component({
+	selector: 'player',
+	template:`
+		<div class="col-lg-1 album-image">
+			<span *ngIf="song.album">
+				<img id="play-image-album" src="{{ url + 'get-image-album/' + song.album.image}}" />
+			</span>
+			<span *ngIf="!song.album">
+				<img if="play-image-album" src="assets/images/default.png" />
+			</span>
+		</div>
+		<div class="col-lg-1 audio-file">
+			<p>Reproduciendo</p>
+			<span id="play-song-title">{{song.name}}</span>
+			<span id="play-song-artist">
+				<span *ngIf="song.artist">{{song.album.artist.name}}</span>
+			</span>
+		</div>
+		<div class="col-lg-10 audio-controls">
+			<audio controls id="player">
+				<source id="mp3-source" src="{{ url + 'get-song-file/' + song.file }}" type="audio/mpeg">
+				Tu navegador no es compatible con HTML5
+			</audio>
+		</div>
+	`
+})
+
+export class  PlayerComponent implements OnInit {
+	public url: string;
+	public song;
+	constructor(){
+		this.url = GLOBAL.url;
+		this.song = new Song(0,'','','','');
+	}
+	ngOnInit(){
+		console.log('player cargado')
+	}
+}
